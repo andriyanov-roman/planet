7a38af96b1684c2e28510cffeff6f2602303b320
randriyanov <randriyanov@mail.ru>
2015-08-09 10:13:03 +0300

    added webapp

---
+++ webapp/web/js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	$(".popup").magnificPopup();
+
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+});
