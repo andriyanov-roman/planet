a5500856ddd635954e5b46eb4885b4753fce1473
Roman Andriyanov <roman.andriyanov@rokittech.com>
2015-08-11 18:08:37 +0300

    added some examples

---
+++ webexamples/src/main/java/planet/entities/Employee.java
@@ -0,0 +1,25 @@
+package planet.entities;
+
+/**
+ * Created by ubn-rok on 11.08.15.
+ */
+public class Employee {
+    private String name;
+    private Long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
