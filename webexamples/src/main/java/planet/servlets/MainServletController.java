21b290750dbae9ae02864e684fd0e44250ec2642
Roman Andriyanov <roman.andriyanov@rokittech.com>
2015-08-11 14:17:52 +0300

    added some examples

---
+++ webexamples/src/main/java/planet/servlets/MainServletController.java
@@ -0,0 +1,51 @@
+package planet.servlets;
+
+import planet.entities.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by ubn-rok on 11.08.15.
+ */
+public class MainServletController extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        String title = "Using GET Method to Read Form Data";
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                        "transitional//en\">\n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "  <li><b>First Name</b>: "
+                + req.getParameter("first_name") + "\n" +
+                "  <li><b>Last Name</b>: "
+                + req.getParameter("last_name") + "\n" +
+                "</ul>\n" +
+                "</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("first_name");
+        User user = new User();
+        user.setName(name);
+        req.setAttribute("user", user);
+        RequestDispatcher view = req.getRequestDispatcher("jsp/result.jsp");
+        view.forward(req, resp);
+    }
+}
