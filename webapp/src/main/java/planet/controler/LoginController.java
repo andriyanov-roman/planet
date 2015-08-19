afb9d46f35b516f01aad21fee8b18519dadcccec
artem <admitriienko@teledes.com.ua>
2015-08-19 09:38:05 +0300

    mvc test

--- webapp/src/main/java/planet/controler/LoginController.java
+++ webapp/src/main/java/planet/controler/LoginController.java
@@ -1,15 +1,24 @@
 package planet.controler;
 
+import planet.entity.User;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /**
  * Created by randriyanov on 09.08.15.
  */
 public class LoginController implements ResponseController {
-
     @Override
     public String getResponse(HttpServletResponse response, HttpServletRequest request) {
-        return null;
+        User user = new User();
+        String login = (String) request.getAttribute("login");
+        String password = (String) request.getAttribute("password");
+        user.setLogin(login);
+        user.setPassword(password);
+        request.setAttribute("user", user);
+        return "PERSONAL_CABINET_ADDR";
+
     }
 }
