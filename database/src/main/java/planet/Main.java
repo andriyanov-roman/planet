52a1ee628a3ddc3fd9df4049405ab1903078ebe0
oleksii <voleksiy@gmail.com>
2015-08-20 12:54:59 +0300

    finance report

--- database/src/main/java/planet/Main.java
+++ database/src/main/java/planet/Main.java
@@ -1,18 +1,31 @@
 package planet;
 
-import planet.dao.UserDaoImpl;
+import planet.dao.*;
 import planet.dao.interfaces_dao.CrudGeneralDao;
-import planet.entity.User;
+import planet.entity.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by oleksii on 09.08.15.
  */
 public class Main {
     public static void main(String[] args){
-        CrudGeneralDao dao = new UserDaoImpl();
-        User u = new User("root");
-        u.setPassword("123");
-        dao.insert(u);
+        FinReportDaoImpl dao = new FinReportDaoImpl();
+        List<FinReport> fr = new ArrayList<FinReport>();
+        fr = dao.select();
+
+        for (int i=0; i<fr.size(); i++)
+        {
+            System.out.print(fr.get(i).getOrderDate()+"\t");
+            System.out.print(fr.get(i).getLogin()+"\t");
+            System.out.print(fr.get(i).getProductName()+"\t");
+            System.out.print(fr.get(i).getProductQty()+"\t");
+            System.out.print(fr.get(i).getAmount()+"\n");
+        }
+
+
     }
 
 }
