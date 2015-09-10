517ac89efeaa491a3503107ac99b4366210240c6
randriyanov <randriyanov@mail.ru>
2015-09-10 20:52:41 +0300

    spring tests

---
+++ spring-test/src/main/java/planet/RootConfig.java
@@ -0,0 +1,15 @@
+package planet;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages={"planet"},
+        excludeFilters={
+                @Filter(type= FilterType.ANNOTATION, value=EnableWebMvc.class)
+        })
+public class RootConfig {
+}
---
+++ spring-test/src/main/java/planet/WebInitializer.java
@@ -0,0 +1,41 @@
+package planet;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by randriyanov on 10.09.15.
+ */
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+
+        DelegatingFilterProxy reconnectDelegate = new DelegatingFilterProxy("apiExceptionHandler");
+
+        return new Filter[] { reconnectDelegate, encodingFilter, new HiddenHttpMethodFilter() };
+    }
+
+}
